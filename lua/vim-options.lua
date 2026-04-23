2609e4743cfc31adb5879078786538dfe738a873
kc <kokchoong611@gmail.com>
2026-04-24 00:22:38 +0800

    add :tabclose

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,8 @@
 -- tabs inserts 2 spaces
-vim.opt.expandtab = true   -- Use spaces instead of tabs
-vim.opt.tabstop = 2        -- Number of spaces a tab counts for
-vim.opt.softtabstop = 2    -- Number of spaces a tab counts for while editing
-vim.opt.shiftwidth = 2     -- Number of spaces used for autoindent
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.tabstop = 2 -- Number of spaces a tab counts for
+vim.opt.softtabstop = 2 -- Number of spaces a tab counts for while editing
+vim.opt.shiftwidth = 2 -- Number of spaces used for autoindent
 
 vim.g.have_nerd_font = true
 vim.g.mapleader = " "
@@ -20,3 +20,6 @@ vim.keymap.set("i", "<S-Tab>", "<C-d>", { desc = "Dedent line" })
 -- in Visual Mode for blocks of code
 vim.keymap.set("v", "<S-Tab>", "<", { desc = "Dedent selection" })
 vim.keymap.set("v", "<Tab>", ">", { desc = "Indent selection" })
+
+-- Tab shortcuts
+vim.keymap.set("n", "tc", ":tabclose<CR>", { desc = "Close tab" })
