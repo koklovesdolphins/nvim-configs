97c170ce3af6c52badbd305ca3f91140e2c31734
kc <kokchoong611@gmail.com>
2026-05-08 00:33:33 +0800

    manual format

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -1,5 +1,7 @@
 local prettier = { "prettierd", "prettier", stop_after_first = true }
 
+local ignore_format_on_save = { markdown = true }
+
 return {
 	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -19,11 +21,19 @@ return {
 	},
 	{
 		"stevearc/conform.nvim",
+		keys = {
+			{
+				"<leader>fmt",
+				function()
+					require("conform").format({ force = true })
+				end,
+				desc = "Force format",
+			},
+		},
 		opts = {
 			formatters_by_ft = {
-				--lua
+				-- lua
 				lua = { "stylua" },
-
 				-- web
 				javascript = prettier,
 				typescript = prettier,
@@ -32,27 +42,25 @@ return {
 				html = prettier,
 				css = prettier,
 				scss = prettier,
-
 				-- code
 				python = { "ruff_organize_imports", "ruff_format" },
 				go = { "goimports", "gofumpt" },
-
-				--shell
+				-- shell
 				sh = { "shfmt" },
-
-				--note
-				-- markdown = prettier,
-
+				-- note
+				markdown = prettier,
 				-- data/config
 				yaml = prettier,
 				toml = { "taplo" },
 				json = prettier,
 				sql = { "sql_formatter" },
 			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+			format_on_save = function(bufnr)
+				if ignore_format_on_save[vim.bo[bufnr].filetype] then
+					return nil
+				end
+				return { timeout_ms = 500, lsp_fallback = true }
+			end,
 		},
 	},
 }
